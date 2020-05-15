6018831e0909bb770245166814d2310f86aaaf6e
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-15 09:47:14 +0530

    two way binding

--- src/App.js
+++ src/App.js
@@ -13,21 +13,25 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'ABC', age: 21 },
-        { name: event.target.value, age: 61 },
-        { name: 'GHI', age: 29 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   tooglePersonHandler = () => {
@@ -51,10 +55,11 @@ class App extends Component {
         < div >
           {this.state.persons.map((person, index) => {
             return <Person
-              click = {() => this.deletePersonHandler(index)}
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
 
         </div>
