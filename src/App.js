bc8cd95f53bd8f6301ea18efeb6fc80e9a9b9855
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-15 09:33:55 +0530

    list intro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'jkl', name: 'ABC', age: 21 },
+      { id: 'ugu', name: 'DEF', age: 61 },
+      { id: 'asf', name: 'GHI', age: 29 }
+    ],
+    otherState: 'ssacsac',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'ABC', age: 21 },
+        { name: event.target.value, age: 61 },
+        { name: 'GHI', age: 29 }
+      ]
+    })
+  };
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  tooglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'green',
+      font: 'inherit',
+      border: '1px solid yellow',
+      padding: '8px',
+      color: 'white',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        < div >
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click = {() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
+          })}
+
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>hello</h1>
+        <button
+          style={style}
+          onClick={this.tooglePersonHandler} >Switch Button</button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default App;
