3c4977a675f5fc4b83e2e75bd49d52e7d9de0363
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-15 23:06:04 +0530

    using Aux in HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 import classesStyle from './Person.module.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component{
     render(){
         console.log("person.js rendering...");
         return (
-            <div className={classesStyle.Person}>
-            <p onClick={this.props.click}>I am {this.props.name} and age is {this.props.age}</p>
-            <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-        )
+            <Aux>
+            <p onClick={this.props.click}>
+                I am {this.props.name} and age is {this.props.age}
+                </p>
+            <p key="i2" >{this.props.children}</p>
+            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+            </Aux>
+        );
     } 
     
 };
