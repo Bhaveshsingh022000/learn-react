008a648704a9cced0370dd8e1c21b3a4049b422f
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-16 14:42:20 +0530

    using ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classesStyle from './Person.module.css';
 import PropTypes from 'prop-types';
 import withClass from '../../../hoc/withClass';
-class Person extends Component{
-    render(){
+class Person extends Component {
+    constructor(props){
+        super(props)
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.inputElementRef.current.focus();
+    }
+
+    render() {
         console.log("person.js rendering...");
         return (
             <React.Fragment>
-            <p onClick={this.props.click}>
-                I am {this.props.name} and age is {this.props.age}
+                <p onClick={this.props.click}>
+                    I am {this.props.name} and age is {this.props.age}
                 </p>
-            <p key="i2" >{this.props.children}</p>
-            <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
+                <p key="i2" >{this.props.children}</p>
+                <input ref={this.inputElementRef} key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
             </React.Fragment>
         );
-    } 
-    
+    }
+
 };
 
 Person.propTypes = {
