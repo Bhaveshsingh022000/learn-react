4d16867861b5fab11c8d9b21394f06e19ca8a29c
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-05-22 20:12:03 +0530

    build controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
-    const transformerdIng = Object.keys(props.ingredients)
+    let transformerdIng = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
                 return <BurgerIngredients key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el)=>{
+            return arr.concat(el)
+        }, []);
+    if(transformerdIng.length === 0){
+        transformerdIng = <p>Please start adding ingredients !</p>;
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
